9268de5810c9d255376068ed783a5c310f3c5e3f
Ekaterina Shitik <shitik.ekaterina@gmail.com>
2024-04-22 14:46:30 +0200

    Group two functions in TestGroupBioSequence

--- test_bioinf_starter_pack.py
+++ test_bioinf_starter_pack.py
@@ -2,15 +2,15 @@ import pytest
 
 from bioinf_starter_pack import DNASequence, AminoAcidSequence
 
+class TestGroupBiolSequence:
+    def test_dna_type(self):
+        inp = 'ATGC'
+        target_type = DNASequence
+        result = DNASequence(inp).complement()
+        assert isinstance(result, target_type)
 
-def test_dna_type():
-    inp = 'ATGC'
-    target_type = DNASequence
-    result = DNASequence(inp).complement()
-    assert isinstance(result, target_type)
 
-
-def test_protein_content():
-    inp = 'BGARB'
-    with pytest.raises(ValueError):
-        AminoAcidSequence(inp)
+    def test_protein_content(self):
+        inp = 'BGARB'
+        with pytest.raises(ValueError):
+            AminoAcidSequence(inp)
