5c14ea94c4c5e754a34a74b6104653f16c58f865
Ekaterina Shitik <shitik.ekaterina@gmail.com>
2024-04-22 14:03:16 +0200

    Make test_dna_type function

--- test_bioinf_starter_pack.py
+++ test_bioinf_starter_pack.py
@@ -1,2 +1,10 @@
 import pytest
 
+from bioinf_starter_pack import DNASequence
+
+
+def test_dna_type():
+    inp = 'ATGC'
+    target_type = DNASequence
+    result = DNASequence(inp).complement()
+    assert isinstance(result, target_type)
